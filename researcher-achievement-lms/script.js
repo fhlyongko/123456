b5b9652e17bf72e8f56a199a6c916f60ca2aec0e
fhlyongko <fhlyongko@gmail.com>
2026-07-20 22:05:18 +0900

    Delete researcher-achievement-lms directory

--- researcher-achievement-lms/script.js
+++
@@ -1,6 +0,0 @@
-const data = window.ACHIEVEMENT_DATA || { totalFiles: 0 };
-const totalFiles = document.querySelector("#totalFiles");
-
-if (totalFiles) {
-  totalFiles.textContent = data.totalFiles.toLocaleString("ko-KR");
-}
